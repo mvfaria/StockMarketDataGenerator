b0b524019ab877842248da61d4f7cc610e02f1d9
Marcus Faria <marcusvinicius.faria@gmail.com>
2024-09-28 00:38:11 +0800

    add more symbols

--- DataGenerator.cs
+++ DataGenerator.cs
@@ -9,23 +9,29 @@ namespace StockMarketDataGenerator
     public class DataGenerator
     {
         private readonly ILogger _logger;
-        private static readonly HttpClient client = new HttpClient();
+        private readonly HttpClient _client;
 
         public DataGenerator(ILoggerFactory loggerFactory)
         {
             _logger = loggerFactory.CreateLogger<DataGenerator>();
+            _client = new HttpClient();
         }
 
         [Function("DataGenerator")]
         public async Task Run([TimerTrigger("*/5 * * * * *")] TimerInfo myTimer)
         {
             var random = new Random();
+
+            var symbolList = new List<string> { "MSFT", "AMZN", "AAPL" };
+            var index = random.Next(symbolList.Count); // Generate a random index
+            var randomSymbol = symbolList[index]; // Pick the symbol at the random index
+
             var stockPrice = 375 + random.Next(-50, 51);  // Price between 325 and 425
 
             var stockData = new
             {
                 datetime = DateTime.UtcNow.ToString("o"),
-                symbol = "MSFT",
+                symbol = randomSymbol,
                 price = stockPrice
             };
 
@@ -33,7 +39,7 @@ namespace StockMarketDataGenerator
             _logger.LogInformation($"Generated Stock Data: {stockJson}");
 
             var stockTradingBotUrl = Environment.GetEnvironmentVariable("StockTradingBot.Url");
-            var response = await client.PostAsync(stockTradingBotUrl, new StringContent(stockJson, System.Text.Encoding.UTF8, "application/json"));
+            var response = await _client.PostAsync(stockTradingBotUrl, new StringContent(stockJson, System.Text.Encoding.UTF8, "application/json"));
             var responseBody = await response.Content.ReadAsStringAsync();
             _logger.LogInformation($"Stock Trading Bot response: {response.StatusCode} - {responseBody}");
         }
